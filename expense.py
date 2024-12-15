b93a45e16edec9e57fb96040ff50918f1bb420c1
ThePrimeagen <samueljamieson02@gmail.com>
2024-12-15 23:49:20 +0000

    started making billsplit

--- expense.py
+++ expense.py
@@ -1,2 +1,12 @@
-amount = input("how much is the bill? ")
-people = input("how many people are there? ")
+def main():
+    amount = input("how much is the bill? ")
+    people = input("how many people are there? ")
+    
+    per_person = billsplit(amount,people)
+
+    print(f"the price per person is {per_person}")
+
+def billsplit(amount,people):
+    return amount / people 
+
+main()
