4a2b8a0eef79f4c2fcb8ce227320216e82a6bcd4
ThePrimeagen <samueljamieson02@gmail.com>
2024-12-15 23:56:51 +0000

    planned out flash cards project

---
+++ flashcard.py
@@ -0,0 +1,9 @@
+#the user is able to add questions and answers 
+
+#each subject will have its own area
+
+#the user will say the difficulty of each questuion
+
+#when testing on a quiz of a subject they will be random
+
+#BONUS if that works see if you can make it so that questions that they struggle are introduced more oftern.
